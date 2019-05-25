bc5ae0320ce34b9d7153d1239d8e118185791fbc
Furtif <msntchat@hotmail.fr>
2019-05-26 00:55:33 +0200

    other fix prevents conflits

--- fs.js
+++ fs.js
@@ -2,6 +2,6 @@ const protobufjs = require('protobufjs');
 const path = require('path');
 
 let root = new protobufjs.Root();
-const protos = root.loadSync(path.join(__dirname, 'proto', 'WUProtos.proto'), { keepCase: true });
+const protos = root.loadSync(path.join(__dirname, 'proto', 'WUProtos.proto.js'), { keepCase: true });
 
 module.exports = protos.WUProtos;
